6e0240d024b0e1411d2a51884ed7180d023436b0
eciggaar <edward@local>
2016-12-01 16:37:21 +0100

    Initial commit

---
+++ samples.go
@@ -0,0 +1,37 @@
+package main
+
+var samples = `
+{
+	"event": {
+		"assetID": "The ID of a managed asset. The resource focal point for a smart contract.",
+		"motorTemp": 160,
+		"currentFloor": 5,
+		"doorOpen": false,
+		"state": "moving",
+		"numberOfFloors": 6,
+		"cabinTemp": 76,
+		"cabinSpeed": 14,
+		"direction": 1,
+		"load": 210,
+		"curtainOfLightBreak": 0,
+		"cleanessOfFloor": 1
+	},
+	"initEvent": {
+		"nickname": "ELEVATOR",
+		"version": "The ID of a managed asset. The resource focal point for a smart contract."
+	},
+	"state": {
+		"assetID": "The ID of a managed asset. The resource focal point for a smart contract.",
+		"motorTemp": 160,
+		"currentFloor": 5,
+		"doorOpen": false,
+		"state": "moving",
+		"numberOfFloors": 6,
+		"cabinTemp": 76,
+		"cabinSpeed": 14,
+		"direction": 1,
+		"load": 210,
+		"curtainOfLightBreak": 0,
+		"cleanessOfFloor": 1
+	}
+}`
